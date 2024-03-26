fec2ae7150a2fb04791eeac3dbba3cab7854dd23
jungwoogyun <jwg8910@naver.com>
2024-03-26 12:06:13 +0900

    .

--- src/Ch31/C03LAMDA.java
+++ src/Ch31/C03LAMDA.java
@@ -9,16 +9,21 @@ int calculate(int num1, int num2);
 public class C03LAMDA {
 	public static void main(String[] args) {
 		
-		Calculator addition = "람다식 정의";
+		Calculator addition =(n1,n2)->n1+n2;
 		System.out.println("Addition: " + addition.calculate(5, 3));
 
-		Calculator subtraction = "람다식 정의";;
+		Calculator subtraction = (n1,n2)->{
+			if(n1>n2)
+				return n1-n2;
+			else
+				return n2-n1;
+		};
 		System.out.println("Subtraction: " + subtraction.calculate(5, 3));	
 		
-		Calculator multiplication = "람다식 정의"; ;
+		Calculator multiplication = (n1,n2)->n1*n2;
 		System.out.println("Multiplication: " + multiplication.calculate(5, 3));
 		
-		Calculator division =  "람다식 정의";;
+		Calculator division = (n1,n2)->n1/n2;
 		System.out.println("Division: " + division.calculate(5, 3));
 		
 	}
