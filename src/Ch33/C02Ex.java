cd98de630dd184c08059e3d00c93e5b49e37e21b
jungwoogyun <jwg8910@naver.com>
2024-03-28 10:05:34 +0900

    .

--- src/Ch33/C02Ex.java
+++ src/Ch33/C02Ex.java
@@ -15,6 +15,11 @@ class Calc{
 		//덧셈로직
 		sum = (numbers)->Arrays.stream(numbers).reduce((a,b)->a+b).getAsInt();
 		//뺄셈로직
+		sub = (numbers)->Arrays.stream(numbers)
+								.sorted()
+								.reduce((a,b)->b-a)
+								.getAsInt();
+								
 		//곱셈로직
 		//나눗셈로직
 	}
@@ -25,7 +30,7 @@ public class C02Ex {
 	public static void main(String[] args) {
 		Calc calc = new Calc();
 		System.out.println("합 :"+ calc.sum.execute(10,20,30,40,50,60));
-		
+		System.out.println("차 :"+ calc.sub.execute(1,4,2,5));//1,2,4,5
 		
 		
 	}
