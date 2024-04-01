a8416f8c87616f1258a988dfd5b8853a18b07753
jungwoogyun <jwg8910@naver.com>
2024-04-01 11:26:32 +0900

    .

--- src/Ch36/Main.java
+++ src/Ch36/Main.java
@@ -1,5 +1,7 @@
 package Ch36;
 
+import java.util.List;
+
 import Ch36.Domain.Dao.BookDaoImpl;
 import Ch36.Domain.Dto.BookDto;
 
@@ -17,7 +19,16 @@ public class Main {
 		
 		//02
 		BookDaoImpl dao = new BookDaoImpl();
-		dao.Insert(new BookDto(1111,"JAVA의정석","EASY","111-111"));
+		dao.Insert(new BookDto(1112,"JAVA의정석-2","EASY","111-112"));
+		dao.Insert(new BookDto(1113,"난정말JAVA를공부해본적이없다구요","00출판사","211-111"));
+		dao.Insert(new BookDto(1114,"JSP/SERVLET 기초","ㅋㅋ출판사","112-111"));
+		
+		//03
+		List<BookDto> list =  dao.SelectAll();
+		list.forEach(dto->{
+			System.out.println(dto);
+		});
+		
 		
 		
 		
