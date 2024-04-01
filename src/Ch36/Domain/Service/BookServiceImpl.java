97bcf89c1c4b92dfdf33fb164d74c94cee681fc1
jungwoogyun <jwg8910@naver.com>
2024-04-01 13:26:57 +0900

    .

--- src/Ch36/Domain/Service/BookServiceImpl.java
+++ src/Ch36/Domain/Service/BookServiceImpl.java
@@ -14,7 +14,7 @@ public class BookServiceImpl {
 	}
 	
 	public boolean bookRegister(BookDto dto) throws Exception {;
-			return dao.Insert(dto);
+		return dao.Insert(dto);
 	};
 	public List<BookDto> getAllBooks() throws Exception {
 		return dao.SelectAll();
